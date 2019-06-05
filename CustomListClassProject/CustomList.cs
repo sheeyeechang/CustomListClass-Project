2967f7b9b18bceb963da3ceab370fdc43a598b3a
Pa Lee <sheeyeechang@gmail.com>
2019-06-05 16:05:40 -0500

    Added AddItem method in the class CustomList

--- CustomListClassProject/CustomList.cs
+++ CustomListClassProject/CustomList.cs
@@ -13,11 +13,11 @@ namespace CustomListClassProject
         private T[] itemArray = new T[5] ;
 
         private int count;
-        // read-only Count property: 
-        public int Count { get { return count; } set { count = value; } }
-
+        // read-only Count property: --- read-only = get 
+        public int Count { get { return count; } }
+         
         private int capacity;
-        // Capacity property: publicly see the size of my private array.
+        // Capacity property: publicly see the size of my private array. ---write = set
         public int Capacity { get { return capacity; } set { count = value; } }
 
 
@@ -31,42 +31,62 @@ namespace CustomListClassProject
         }
 
         // Member method
-        //Get Set Array
+        //ADD TESTS: 6 total 
+        //Add method: adds an input value to an array.
 
-            // C# indexer so that I can make the objects in my list accessible via index. A user cannot access an out-of-bounds index.
+            public void AddItem(T item)
+            {
+                CustomList<T> testList = new CustomList<T>();
+            if (count == capacity)
+            {
+                for (int i = 0; i < count; i++)
+                {
+                    itemArray = new T[count];
+                }
+            }
+            else
+            {
 
-            // method ability to add an object 
-
-
-
-            // method ability to remove an object
-
-
-
-
-
-            // override the ToString method that converts the contents of the custom list to a string.
-
-            // be able to overload the + operator, so that I can add two instances of the custom list class together.
-            //List<int> one = new List<int>() {1,3,5}; and List<int> two = new List<int>() { 2, 4, 6 };
-            //List<int> result = one + two;
-            //result has 1,3,5,2,4,6
-
-            //, I want to be able to overload the – operator, so that I can subtract one instance of a custom list class from another instance of a custom list class.
-            //List<int> one = new List<int>() {1,3,5}; and List<int> two = new List<int>() { 2, 1, 6 };
-            //List<int> result = one - two;
-            //result has 3,5
-
-            // I want the ability to zip two custom list class instances together in the form of a zipper. An example:
-            //I have List<int> odd = new List<int>() {1,3,5}; and List<int> even = new List<int>() { 2, 4, 6 }; 
-            //odd.Zip(even);
-            //When lists odd and even are zipped together, your new list will contain values 1,2,3,4,5,6
-
-            //custom list class to be iterable.
-
-            //
+            }
+            }
+            
 
 
-        }
+
+        // C# indexer so that I can make the objects in my list accessible via index. A user cannot access an out-of-bounds index.
+
+        // method ability to add an object 
+
+
+
+        // method ability to remove an object
+
+
+
+
+
+        // override the ToString method that converts the contents of the custom list to a string.
+
+        // be able to overload the + operator, so that I can add two instances of the custom list class together.
+        //List<int> one = new List<int>() {1,3,5}; and List<int> two = new List<int>() { 2, 4, 6 };
+        //List<int> result = one + two;
+        //result has 1,3,5,2,4,6
+
+        //, I want to be able to overload the – operator, so that I can subtract one instance of a custom list class from another instance of a custom list class.
+        //List<int> one = new List<int>() {1,3,5}; and List<int> two = new List<int>() { 2, 1, 6 };
+        //List<int> result = one - two;
+        //result has 3,5
+
+        // I want the ability to zip two custom list class instances together in the form of a zipper. An example:
+        //I have List<int> odd = new List<int>() {1,3,5}; and List<int> even = new List<int>() { 2, 4, 6 }; 
+        //odd.Zip(even);
+        //When lists odd and even are zipped together, your new list will contain values 1,2,3,4,5,6
+
+        //custom list class to be iterable.
+
+        //
+
+
     }
+
 }
--- CustomListClassProjectUnitTest/UnitTest1.cs
+++ CustomListClassProjectUnitTest/UnitTest1.cs
@@ -6,17 +6,18 @@ namespace CustomListClassProjectUnitTest
     [TestClass]
     public class UnitTest1
     {
-        //ADD TESTS: 5 total
+        //ADD TESTS: 6 total
         [TestMethod]
         public void Add_AddItemIntToEmptyList_ItemGoesToIndexZero()
         {
             // arrange
             CustomList<int> testList = new CustomList<int>();
+            int value1 = 4;
             int expected = 4;
             int actual;
 
             // act
-            testList.Add(expected);
+            testList.Add(value1);
             actual = testList.arrayItem[0];
 
             // assert
@@ -27,11 +28,12 @@ namespace CustomListClassProjectUnitTest
         {
             // arrange
             CustomList<string> testList = new CustomList<string>();
-            int expected = "Mike";
-            int actual;
+            string value1 = "Mike";
+            string expected = "Mike";
+            string actual;
 
             // act
-            testList.Add(expected);
+            testList.Add(value1);
             actual = testList.itemArray[0];
 
             // assert
@@ -43,11 +45,12 @@ namespace CustomListClassProjectUnitTest
         {
             // arrange
             CustomList<int> testList = new CustomList<int>();
+            int value1 = 1;
             int expected = 1;
             int actual;
 
             // act
-            testList.Add(698);
+            testList.Add(value1);
             actual = testList.Count;
 
             // assert
@@ -62,6 +65,8 @@ namespace CustomListClassProjectUnitTest
             //Add multiple items to an empty list
         public void Add_AddThreeItemsIntToEmptyList_ReturnThreeItemListCount()
         {
+
+
             // arrange
             CustomList<int> testList = new CustomList<int>();
             int value1 = 1; 
@@ -74,7 +79,7 @@ namespace CustomListClassProjectUnitTest
             testList.Add(value1);
             testList.Add(value2);
             testList.Add(value3);
-            actual = testList[0] + testList[2] + testList[3]; // testList.Count;
+            actual = testList.Count;
 
             // assert
             Assert.AreEqual(expected, actual);
@@ -110,6 +115,35 @@ namespace CustomListClassProjectUnitTest
             // assert
             Assert.AreEqual(expected, actual);
         }
+        // Test if value in the right spot
+        public void Add_AddItemsIntGreaterThanCapacity_ReturnItemListCountSort()
+        {
+            // arrange
+            CustomList<int> testList = new CustomList<int>();
+            int value1 = 2;
+            int value2 = 4;
+            int value3 = 6;
+            int value4 = 8;
+            int value5 = 10;
+            int value6 = 12;
+            int value7 = 14;
+            int value8 = 16;
+            int expected = 16;
+            int actual;
 
+            // act
+            testList.Add(value1);
+            testList.Add(value2);
+            testList.Add(value3);
+            testList.Add(value4);
+            testList.Add(value5);
+            testList.Add(value6);
+            testList.Add(value7);
+            testList.Add(value8);
+            actual = testList[7];
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
